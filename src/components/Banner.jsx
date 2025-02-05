b7d1e621da9ac44ff3cf34a84f0c8f97897c1973
MAvishkar12 <moreavishkar12@gamil.com>
2025-02-05 16:13:48 +0530

    Image add in assets folder

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,11 @@
+import hero from "../assets/hero.png"
 function Banner() {
   return (
     <div
       className="  h-screen md:h-[80vh] sm:h-[60vh] opacity-100 bg-cover flex items-end "
     
      >
-      <img  className="m-auto" src="../../public/hero.png"></img>
+      <img  className="m-auto" src={hero} alt="Poster"></img>
      
     </div>
   );
